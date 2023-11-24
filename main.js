5703cceabc90ab159cdc69f936c5ca75b42c5c52
KristinaBBB <148815348+KristinaBBB@users.noreply.github.com>
2023-11-24 17:00:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 
 function setToTeacher(){
   const fs = require('fs');
-  let username = document.getElementById("textfield1).value;
-  let password = document.getElementById("textfield2).value;
+  let username = document.getElementById("textfield1").value;
+  let password = document.getElementById("textfield2").value;
   let currentTeacher = username + ";" + password;
 
 fs.readFile('teachers.txt', 'utf8', (err, data) => {
@@ -11,7 +11,7 @@ fs.readFile('teachers.txt', 'utf8', (err, data) => {
     console.error('Error reading the file:',err);
     return;
   }
-  const updatedContent = data + newLine + CurrentTeacher;
+  const updatedContent = data + newLine + currentTeacher;
 
   fs.writeFile('teachers.txt', updatedContent, 'utf8', (err) =>{
      if (err){
@@ -35,7 +35,7 @@ fs.readFile('students.txt', 'utf8', (err, data) => {
     console.error('Error reading the file:',err);
     return;
   }
-  const updatedContent = data + newLine + CurrentStudent;
+  const updatedContent = data + '\n' + currentStudent;
 
   fs.writeFile('students.txt', updatedContent, 'utf8', (err) =>{
      if (err){
