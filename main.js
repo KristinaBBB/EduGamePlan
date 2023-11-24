e6c4c96d29db57446936ab95eaeef0f9b2864231
349633081 <148815378+349633081@users.noreply.github.com>
2023-11-24 17:11:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,45 @@
-
-const fs = require('fs');
-function setToTeacher(){
-  
+function setToTeacher() {
+  const fs = require('fs');
   let username = document.getElementById("textfield1").value;
   let password = document.getElementById("textfield2").value;
   let currentTeacher = username + ";" + password;
-
-fs.readFile('teachers.txt', 'utf8', (err, data) => {
-  if (err){
-    console.error('Error reading the file:',err);
-    return;
-  }
-  const updatedContent = data + '\n' + currentTeacher;
-
-  fs.writeFile('teachers.txt', updatedContent, 'utf8', (err) =>{
-     if (err){
-      console.error('Error writing to the file:',err);
+  
+  fs.readFile('teachers.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading the file:', err);
       return;
-  }
+    }
+    const updatedContent = data + '\n' + currentTeacher;
+  
+    fs.writeFile('teachers.txt', updatedContent, 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing to the file:', err);
+        return;
+      }
+      console.log('String added to the file successfully!');
+    });
   });
-});
 }
 
-
-function setToStudent(){
- 
-  let username = document.getElementById("textfield1).value;
-  let password = document.getElementById("textfield2).value;
+function setToStudent() {
+  const fs = require('fs');
+  let username = document.getElementById("textfield1").value;
+  let password = document.getElementById("textfield2").value;
   let currentStudent = username + ";" + password;
-
-fs.readFile('students.txt', 'utf8', (err, data) => {
-  if (err){
-    console.error('Error reading the file:',err);
-    return;
-  }
-  const updatedContent = data + '\n' + currentStudent;
-
-  fs.writeFile('students.txt', updatedContent, 'utf8', (err) =>{
-     if (err){
-      console.error('Error writing to the file:',err);
+  
+  fs.readFile('students.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading the file:', err);
       return;
-  }
-    console.log('String added to  the file successfully!');
+    }
+    const updatedContent = data + '\n' + currentStudent;
+  
+    fs.writeFile('students.txt', updatedContent, 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing to the file:', err);
+        return;
+      }
+      console.log('String added to the file successfully!');
+    });
   });
-});
 }
