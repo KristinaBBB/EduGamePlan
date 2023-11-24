86de92cc5ae845febf983126b47bbec7ecb017a4
KristinaBBB <148815348+KristinaBBB@users.noreply.github.com>
2023-11-24 16:20:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+document.getElementById("textfield1".addEventListener("keydown", function(event){
+  if (event.key === "Enter"){
+    getUserInput(); }
+});
+
 function myFunction(){
   let username = document.getElementById("textfield1).value;
   document.getElementById("displaInput").innerText = "You entered: " + userInputValue;
