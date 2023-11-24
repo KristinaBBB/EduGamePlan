f7888c16bb9b879235c618482547eb5e3e1f7362
KristinaBBB <148815348+KristinaBBB@users.noreply.github.com>
2023-11-24 16:53:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -22,3 +22,27 @@ fs.readFile('teachers.txt', 'utf8', (err, data) => {
   });
 });
 }
+
+
+function setToStudent(){
+  const fs = require('fs');
+  let username = document.getElementById("textfield1).value;
+  let password = document.getElementById("textfield2).value;
+  let currentStudent = username + ";" + password;
+
+fs.readFile('students.txt', 'utf8', (err, data) => {
+  if (err){
+    console.error('Error reading the file:',err);
+    return;
+  }
+  const updatedContent = data + newLine + CurrentStudent;
+
+  fs.writeFile('students.txt', updatedContent, 'utf8', (err) =>{
+     if (err){
+      console.error('Error writing to the file:',err);
+      return;
+  }
+    console.log('String added to  the file successfully!');
+  });
+});
+}
