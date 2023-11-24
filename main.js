df8051514adb6b345c17e9cac64be9b11966844e
KristinaBBB <148815348+KristinaBBB@users.noreply.github.com>
2023-11-24 16:43:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-function myFunction(){
+const fs = require('fs');
+
+function setToTeacher(){
   let username = document.getElementById("textfield1).value;
-  document.getElementById("displaInput").innerText = "You entered: " + userInputValue;
+  let password = document.getElementById("textfield2).value;
+  let currentTeacher = username + ";" + password;
 }
+fs.readFile('teachers.txt', 'utf8', (err, data) => {
+  if (err){
+    console.error('Error reading the file:',err);
+    return;
+  }
+  const updatedContent = data + newLine + CurrentTeacher;
+
+  fs.writeFile('teachers.txt', updatedContent, 'utf8', (err) =>{
+     if (err){
+      console.error('Error writing to the file:',err);
+      return;
+  }
+    console.log('String added to  the file successfully!');
+  });
+});
